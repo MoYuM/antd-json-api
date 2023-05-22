6a3fda192650595b67dfa80ba705cdf34f7c0474
MoYuM <hanbaochang12138@gmail.com>
2023-05-22 22:48:09 +0800

    parse markdown

--- index.js
+++ index.js
@@ -1,40 +1,78 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 const path = require('path');
-const marked = require('marked')
+const { marked } = require('marked')
 
 const ANTD_GIT_URL = 'https://github.com/ant-design/ant-design.git';
+const OUT_PUT = 'api.json';
 
+const lexer = new marked.Lexer()
 // 克隆仓库
-exec(`git clone ${ANTD_GIT_URL}`, (err) => {
+// exec(`git clone ${ANTD_GIT_URL}`, (err) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   console.log('仓库克隆成功！');
+// });
+
+const obj = {
+  componentList: []
+}
+
+// 读取组件文件夹下的文档
+fs.readdir('./ant-design/components', { withFileTypes: true }, (err, files) => {
   if (err) {
-    console.log(err);
-    return;
-  }
-  console.log('仓库克隆成功！');
+    console.log('error', error);
+  };
+
+  // 遍历文件夹
+  files.forEach((file) => {
+    if (file.isDirectory()) {
+      const filePath = path.join('./ant-design/components', file.name, 'index.zh-CN.md');
+
+      // 读取文档内容
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          return;
+        };
+
+        // TODO 
+        // if (file.name !== 'button') return;
+
+        const api = [];
+
+        // 解析 Markdown
+        const ast = lexer.lex(data)
+        ast.filter(i => i.type === 'table').forEach(i => {
+          i.rows.forEach(row => {
+            api.push({
+              "name": row[0]?.text,
+              "desc": row[1]?.text,
+              "type": row[2]?.text,
+              "default": row[3]?.text || '-'
+            })
+          })
+        })
+
+        obj.componentList.push({
+          component: file.name,
+          api,
+        })
+
+        writeFile(JSON.stringify(obj));
+      });
+    }
+  });
 });
 
-  // 读取组件文件夹下的文档
-  fs.readdir('./ant-design/components', { withFileTypes: true }, (err, files) => {
-    if (err)  {
-      console.log('error', error);
-    };
 
-    // 遍历文件夹
-    files.forEach((file) => {
-      if (file.isDirectory()) {
-        const filePath = path.join('./ant-design/components', file.name, 'index.zh-CN.md');
-
-        // 读取文档内容
-        fs.readFile(filePath, 'utf8', (err, data) => {
-          if (err) {
-            return;
-          };
-
-          // 解析 Markdown
-          console.log(file.name);
-          console.log(marked.Lexer(data.replace(/---[\s\S]*---/, '')));
-        });
-      }
-    });
+function writeFile(string) {
+  fs.writeFile(OUT_PUT, string, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing file:', err);
+    } else {
+      console.log('File successfully written!');
+    }
   });
+}
