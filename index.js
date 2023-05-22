4db608053119d57a52efdd176682607d5705d28f
MoYuM <hanbaochang12138@gmail.com>
2023-05-22 20:37:47 +0800

    add gitignore

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const { exec } = require('child_process');
 const fs = require('fs');
+const path = require('path');
+const marked = require('marked')
+
+const ANTD_GIT_URL = 'https://github.com/ant-design/ant-design.git';
 
 // 克隆仓库
-exec('git clone https://github.com/ant-design/ant-design.git', (err) => {
+exec(`git clone ${ANTD_GIT_URL}`, (err) => {
   if (err) {
     console.log(err);
     return;
   }
   console.log('仓库克隆成功！');
-  
-  // 读取复制的文件路径
-  fs.readFile('./ant-design/README.md', 'utf8', (err, data) => {
-    if(err) {
-      console.log(err);
-      return;
-    }
-    
-    // 将文件内容复制到指定目录下
-    fs.writeFile('./README.md', data, (err) => {
-      if(err) {
-        console.log(err);
-        return;
+});
+
+  // 读取组件文件夹下的文档
+  fs.readdir('./ant-design/components', { withFileTypes: true }, (err, files) => {
+    if (err)  {
+      console.log('error', error);
+    };
+
+    // 遍历文件夹
+    files.forEach((file) => {
+      if (file.isDirectory()) {
+        const filePath = path.join('./ant-design/components', file.name, 'index.zh-CN.md');
+
+        // 读取文档内容
+        fs.readFile(filePath, 'utf8', (err, data) => {
+          if (err) {
+            return;
+          };
+
+          // 解析 Markdown
+          console.log(file.name);
+          console.log(marked.Lexer(data.replace(/---[\s\S]*---/, '')));
+        });
       }
-      console.log('README.md 文件复制成功！');
     });
   });
-});
