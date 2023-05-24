04a3060d83d5934510ee81d954d9a1101b410c0f
MoYuM <hanbaochang12138@gmail.com>
2023-05-24 18:51:20 +0800

    goon

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ANTD_GIT_URL = 'https://github.com/ant-design/ant-design.git';
 const OUT_PUT = 'api.json';
 
 const lexer = new marked.Lexer()
+
 // 克隆仓库
 // exec(`git clone ${ANTD_GIT_URL}`, (err) => {
 //   if (err) {
@@ -14,58 +15,97 @@ const lexer = new marked.Lexer()
 //     return;
 //   }
 //   console.log('仓库克隆成功！');
-// });
 
+// });
 const obj = {
   componentList: []
 }
 
+
 // 读取组件文件夹下的文档
 fs.readdir('./ant-design/components', { withFileTypes: true }, (err, files) => {
   if (err) {
-    console.log('error', error);
+    console.log('error', err);
   };
 
-  // 遍历文件夹
-  files.forEach((file) => {
-    if (file.isDirectory()) {
-      const filePath = path.join('./ant-design/components', file.name, 'index.zh-CN.md');
+  const promise = []
 
-      // 读取文档内容
+  // 遍历文件夹
+  for (const file of files) {
+    if (!file.isDirectory()) continue;
+
+    const filePath = path.join('./ant-design/components', file.name, 'index.zh-CN.md');
+
+    promise.push(new Promise((resolve, reject) => {
       fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
+          reject(err)
           return;
         };
-
-        // TODO 
-        // if (file.name !== 'button') return;
-
-        const api = [];
-
-        // 解析 Markdown
-        const ast = lexer.lex(data)
-        ast.filter(i => i.type === 'table').forEach(i => {
-          i.rows.forEach(row => {
-            api.push({
-              "name": row[0]?.text,
-              "desc": row[1]?.text,
-              "type": row[2]?.text,
-              "default": row[3]?.text || '-'
-            })
-          })
-        })
-
+        const {api, children} = readFile(data)
         obj.componentList.push({
           component: file.name,
+          children,
           api,
         })
+        resolve()
+      })
+    }))
+  };
 
-        writeFile(JSON.stringify(obj));
-      });
-    }
-  });
+  Promise.allSettled(promise).then(() => {
+    writeFile(JSON.stringify(obj))
+  }).catch(err => {
+    console.log('err ======>', err)
+  })
 });
 
+
+function readFile(data) {
+  const tokens = lexer.lex(data)
+  const children = [];
+  const api = [];
+  let isAPI = false;
+
+  // console.log('tokens', tokens);
+  tokens.forEach(i => {
+    if (i.type === 'heading' && (i.text === 'API')) {
+      isAPI = true;
+      return;
+    }
+
+    if (isAPI && i.type === 'heading' && (i.depth === 3)) {
+      children.push({
+        name: i.text,
+        api: []
+      })
+    }
+
+    if (isAPI && i.type === 'table') {
+      const apiList = i.rows.map(row => ({
+        "name": row[0]?.text,
+        "desc": row[1]?.text,
+        "type": row[2]?.text,
+        "default": row[3]?.text || '-'
+      }))
+      if (children.length) {
+        children[children.length - 1].api = apiList;
+      } else {
+        api.push(...apiList);
+      }
+      return;
+    }
+
+    if (isAPI && (i.type === 'heading') && (i.depth === 2) && (i.text !== 'API')) {
+      isAPI = false;
+      return;
+    }
+  })
+  return {
+    api,
+    children
+  }
+}
 
 function writeFile(string) {
   fs.writeFile(OUT_PUT, string, 'utf-8', (err) => {
